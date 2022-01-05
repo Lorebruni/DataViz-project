95790d786f7a309552c9e73d9a3ca77e680ced02
Lorebruni <97045658+Lorebruni@users.noreply.github.com>
2022-01-06 00:09:13 +0100

    Add files via upload

--- DataVizProject1.py
+++ DataVizProject1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[9]:
 
 
 import pandas as pd
@@ -11,7 +11,7 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 
-# In[2]:
+# In[11]:
 
 
 line=pd.read_csv('line.csv')
@@ -19,13 +19,13 @@ wdi_scatter=pd.read_csv('wdi_scatter.csv')
 wdi_new1=pd.read_csv('wdi_new1.csv')
 
 
-# In[6]:
+# In[12]:
 
 
 clist=wdi_new1["Country Name"].unique()
 
 
-# In[7]:
+# In[13]:
 
 
 st.set_page_config(layout = "wide")
@@ -39,6 +39,14 @@ fig = px.line(wdi_new1[wdi_new1["Country Name"] == country], x="Year", y="Percen
              color='Indicator Name', markers=True, range_y=[0,100],
              title='Drinking water services for rural, urban and total population',
              template="simple_white")
+newnames = {'% of total population using drinking water services':'total population', 
+            '% of rural population using drinking water services': 'rural population',
+            '% of urban population using drinking water services': 'urban population'}
+fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
+                                      legendgroup = newnames[t.name],
+                                      hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])
+                                     )
+                  )
 col1.plotly_chart(fig,use_column_width = True)
 
 selected=wdi_scatter[wdi_scatter["Country Name"] == country]
@@ -56,10 +64,4 @@ reference_line = go.Scatter(x=selected2['Year'],
 fig1.add_trace(reference_line)
 
 col2.plotly_chart(fig1,use_column_width = True)
-
-
-# In[ ]:
-
-
-
 

98b3689fa5363c1425151f5beb3b8f387c3d34b4
marco-scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-01-06 22:17:35 +0100

    Update DataVizProject1.py

--- DataVizProject1.py
+++ DataVizProject1.py
@@ -90,12 +90,12 @@ if graph == 'Time series':
     col2.plotly_chart(fig1,use_column_width = True)
   
 elif graph=='Geografical Map':
-    def map():
-        return(components.html(source_code, height = 700, scrolling=False))
+    
     st.sidebar.info('Remember to close the sidebar if you want to interact with the map :)')
     st.sidebar.warning("If you can't see the slider to select year, or you see it badly, try to change broswer and use Chrome or Firefox")
     with st.container():
         st.title('Africa')
-        map()
+        components.html(source_code, height = 700, scrolling=False)
+        
  
 

166a3fe3888050f7952bfe2dcdfacd6ad8451a98
marco-scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-01-06 21:54:00 +0100

    Update DataVizProject1.py

--- DataVizProject1.py
+++ DataVizProject1.py
@@ -91,7 +91,7 @@ if graph == 'Time series':
 elif graph=='Geografical Map':
     @st.cache
     def map():
-        m=components.html(func=source_code, height = 700, scrolling=False)
+        m=components.html(source_code, height = 700, scrolling=False)
         return(m)
     st.sidebar.info('Remember to close the sidebar if you want to interact with the map :)')
     st.sidebar.warning("If you can't see the slider to select year, or you see it badly, try to change broswer and use Chrome or Firefox")

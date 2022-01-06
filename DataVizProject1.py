429d33b4cb3e4a60d2d354d428e8abfa3192b103
marco-scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-01-06 20:57:34 +0100

    Update DataVizProject1.py

--- DataVizProject1.py
+++ DataVizProject1.py
@@ -28,40 +28,67 @@ clist=wdi_new1["Country Name"].unique()
 # In[14]:
 
 
-st.set_page_config(layout = "wide")
-country = st.sidebar.selectbox('Select a Country:',clist)
-st.header("**National Statistics for African states**")
+st.set_page_config(layout = "wide",initial_sidebar_state="collapsed")
 
-col1, col2 = st.columns(2)
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        width: 300px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        width: 300px;
+        margin-left: -500px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-fig = px.line(wdi_new1[wdi_new1["Country Name"] == country], x="Year", y="Percentage", 
-             labels=dict(x = 'Years', y = 'Percentage of people using drinking water services'), 
-             color='Indicator Name', markers=True, range_y=[0,100],
-             title='Drinking water services for rural, urban and total population',
-             template="simple_white")
-newnames = {'% of total population using drinking water services':'total population', 
-            '% of rural population using drinking water services': 'rural population',
-            '% of urban population using drinking water services': 'urban population'}
-fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
-                                      legendgroup = newnames[t.name],
-                                      hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])
-                                     )
-                  )
-col1.plotly_chart(fig,use_column_width = True)
 
-selected=wdi_scatter[wdi_scatter["Country Name"] == country]
-fig1 = px.line(selected, 
-              x = 'Year', y = 'Value', 
-              labels=dict(x ='Years', y= 'Number of people'), color='Indicator Name', markers=True,
-              title = "Population growth vs Population with drinking services growth",
-              template="simple_white")
+graph = st.sidebar.selectbox('What type of graph',['Geografical Map','Time series'])
 
-selected2=line[line["Country Name"] == country]
-reference_line = go.Scatter(x=selected2['Year'],
-                            y=selected2['Value'],
-                            mode="lines", name="N. people with drinking water services if only population grow",
-                            line=go.scatter.Line(color="gray"))
-fig1.add_trace(reference_line)
+if graph == 'Time series':
+  country = st.sidebar.selectbox('Select a Country:',clist)
+  st.header("**National Statistics for African states**")
 
-col2.plotly_chart(fig1,use_column_width = True)
+  col1, col2 = st.columns(2)
+
+  fig = px.line(wdi_new1[wdi_new1["Country Name"] == country], x="Year", y="Percentage", 
+               labels=dict(x = 'Years', y = 'Percentage of people using drinking water services'), 
+               color='Indicator Name', markers=True, range_y=[0,100],
+               title='Drinking water services for rural, urban and total population',
+               template="simple_white")
+  newnames = {'% of total population using drinking water services':'total population', 
+              '% of rural population using drinking water services': 'rural population',
+              '% of urban population using drinking water services': 'urban population'}
+  fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
+                                        legendgroup = newnames[t.name],
+                                        hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])
+                                       )
+                    )
+  col1.plotly_chart(fig,use_column_width = True)
+
+  selected=wdi_scatter[wdi_scatter["Country Name"] == country]
+  fig1 = px.line(selected, 
+                x = 'Year', y = 'Value', 
+                labels=dict(x ='Years', y= 'Number of people'), color='Indicator Name', markers=True,
+                title = "Population growth vs Population with drinking services growth",
+                template="simple_white")
+
+  selected2=line[line["Country Name"] == country]
+  reference_line = go.Scatter(x=selected2['Year'],
+                              y=selected2['Value'],
+                              mode="lines", name="N. people with drinking water services if only population grow",
+                              line=go.scatter.Line(color="gray"))
+  fig1.add_trace(reference_line)
+
+  col2.plotly_chart(fig1,use_column_width = True)
+  
+elif graph=='Geografical Map':
+  st.sidebar.info('Remember to close the sidebar if you want to interact with the map :)')
+        with st.container():
+                st.title('Africa')
+                components.html(source_code, height = 700, scrolling=False)
+ 
 

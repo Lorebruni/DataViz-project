232968938916bfc8759b32e46ef152f7d18bdc2d
Lorebruni <97045658+Lorebruni@users.noreply.github.com>
2022-01-09 17:02:32 +0100

    Add files via upload

--- DataVizProject1.py
+++ DataVizProject1.py
@@ -65,7 +65,10 @@ if graph == 'Time series':
                    labels=dict(x = 'Years', y = 'Percentage of people using drinking water services'), 
                    color='Indicator Name', markers=True, range_y=[0,100],
                    title='Drinking water services for rural, urban and total population',
-                   template="seaborn")
+                   template="seaborn", 
+                   category_orders={"Indicator Name": ['% of urban population using drinking water services',
+                                                       '% of total population using drinking water services',
+                                                       '% of rural population using drinking water services']})
     newnames = {'% of urban population using drinking water services': '% of urban population',
                 '% of total population using drinking water services':'% of total population', 
                 '% of rural population using drinking water services': '% of rural population'}
@@ -74,9 +77,6 @@ if graph == 'Time series':
                                           hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])
                                            )
                         )
-    fig.update_layout(category_orders={"Indicator Name": ['% of urban population',
-                                                          '% of total population',
-                                                          '% of rural population']})
     col1.plotly_chart(fig,use_column_width = True)
 
     selected=wdi_scatter[wdi_scatter["Country Name"] == country]

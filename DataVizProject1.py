2b08e0a85778d2d575928727e01f727322cee8bc
marco-scatassi <96434607+marco-scatassi@users.noreply.github.com>
2022-01-15 21:21:02 +0100

    Update DataVizProject1.py

--- DataVizProject1.py
+++ DataVizProject1.py
@@ -59,7 +59,7 @@ if graph == 'Time series':
     st.write('''In this section you can explore in more detail the change over time for a specif country, only states with information available for each year since 2000 are 
               selectable.''')
 
-    col1, col2, col3 = st.columns([1,3,1])
+    col1, col2 = st.columns(2)
 
     fig = px.line(wdi_new1[wdi_new1["Country Name"] == country], x="Year", y="Percentage (%)", 
                    labels=dict(x = 'Years', y = 'Percentage of people using drinking water services'), 
@@ -77,7 +77,7 @@ if graph == 'Time series':
                                           hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])
                                            )
                         )
-    col2.plotly_chart(fig,use_column_width = True)
+    col1.plotly_chart(fig,use_column_width = True)
 
     selected=wdi_scatter[wdi_scatter["Country Name"] == country]
     fig1 = px.line(selected, 
